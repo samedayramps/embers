6be297a6b60f972d0754561eb1db48783dedbd0f
samedayramps <ty@samedayramps.com>
2024-11-11 23:26:46 -0600

    feat: add netlify edge integration

--- src/root.tsx
+++ src/root.tsx
@@ -12,8 +12,27 @@ export default component$(() => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>daisyUI + Qwik</title>
+        
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-N4MHK8RN');`} />
+        {/* End Google Tag Manager */}
       </head>
       <body lang="en">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N4MHK8RN"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <RouterOutlet />
       </body>
     </QwikCityProvider>
